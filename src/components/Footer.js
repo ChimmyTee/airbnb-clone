bad0e41f94d5c776fd5273b822a4ca6f15dcfad1
Chimmy <jimmyeetran@gmail.com>
2023-03-17 01:16:41 +1100

    changed from grid to flex for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 md:px-50 lg:px-60'>
+    // <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 md:px-50 lg:px-60'>
+    <div className='flex justify-evenly py-14 px-5 bg-gray-50'>
         <div className='space-y-4 text-gray-800'>
             <h5 className='font-bold'>ABOUT</h5>
             <p>How Airbnb works</p>
