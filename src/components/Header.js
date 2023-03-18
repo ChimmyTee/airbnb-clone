35cc8c67946523aa97c3dc0cb092a4dc62e98552
Chimmy <jimmyeetran@gmail.com>
2023-03-18 23:03:16 +1100

    added useState to search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import HeaderImage from 'next/image'
 import React from 'react'
+import { useState } from 'react'
 import { MagnifyingGlassIcon, UserCircleIcon, Bars3Icon } from '@heroicons/react/24/solid'
 import { GlobeAltIcon } from '@heroicons/react/24/outline'
 
 
 
 function Header() {
+  const [searchInput, setSearchInput] = useState('')
+  console.log(searchInput)
+
   return (
     // Import to use header tag for SEO purposes
     <header className='sticky top-0 z-50 p-4 shadow-md flex justify-between bg-white bg-transparent'>
@@ -22,7 +26,8 @@ function Header() {
       </div>
 
       <div className="flex max-w-md grow items-center justify-between border-2 rounded-full px-3 mx-2 shadow-sm">
-        <input type='text' placeholder='Start your search' className='outline-none text-gray-600' />
+        <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)}
+            type='text' placeholder='Start your search' className='outline-none text-gray-600' />
         <MagnifyingGlassIcon className='h-8 bg-red-400 text-white rounded-full p-2 ml-2 cursor-pointer' />
       </div>
 
