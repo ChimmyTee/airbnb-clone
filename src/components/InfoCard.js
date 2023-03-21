cdce70c226fe11b9a056e4e8727b3bd87ad580e2
Chimmy <jimmyeetran@gmail.com>
2023-03-21 21:05:44 +1100

    added loading bar at the top and centered search pictures

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -7,7 +7,7 @@ function InfoCard({ img, location, title, description, star, price, total }) {
   return (
     <div className='flex py-7 px-2 border-b cursor-pointer hover:opacity-90 hover:shadow-lg
       transition duration-200 ease-out first:border-t'>
-      <div className='relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0'>
+      <div className='relative my-auto h-40 w-56 md:h-52 md:w-80 flex-shrink-0'>
         <Image src={img} fill className='object-cover'/>
       </div>
       <div className='flex flex-col flex-grow pl-4 pt-1'>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,22 @@
 import '@/styles/globals.css'
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from 'next/router';
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FE595E',
+  className: "z-50",
+  delay: 100,
+});
+
+// fire the progress.start when the routerChange just Started.
+Router.events.on('routeChangeStart', progress.start);
+
+// finish the progress when the routerChange is complete.
+Router.events.on('routeChangeComplete', progress.finish);
+
+// if there's an error on the route change, finish progress to terminate it.
+Router.events.on('routeChangeError', progress.finish);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
